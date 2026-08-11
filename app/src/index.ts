574d823e8f3ae34431990bf26019b81fd4337d90
kouphasi <phasi.ok.phasi@gmail.com>
2026-08-11 19:20:24 +0900

    add: prom-client to app

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,13 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { prometheus } from '@hono/prometheus'
 
 const app = new Hono()
+
+const { printMetrics, registerMetrics } = prometheus()
+
+app.use('*', registerMetrics)
+app.get('/metrics', printMetrics)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
