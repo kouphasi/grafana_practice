0252b696ca9f7bb3992037b91e67812a1a4e8593
kouphasi <phasi.ok.phasi@gmail.com>
2026-08-11 19:03:22 +0900

    update: runner from bun to node

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,15 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-});
+})
 
-
-export default {
-  port: 8080,
+serve({
   fetch: app.fetch,
-}
+  port: 8080
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
